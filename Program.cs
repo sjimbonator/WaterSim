ef705bfcd6bdf2a39c15e74bb0519484e97a7588
Sjimmie <sjimmie.bleeker@student.nhlstenden.com>
2020-08-13 00:26:57 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WaterSim
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

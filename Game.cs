63f94ebea8cc8b95905ecf630895e4ac22c02d74
Sjimmie <sjimmie.bleeker@student.nhlstenden.com>
2020-08-13 13:45:39 +0200

    biem

---
+++ Game.cs
@@ -0,0 +1,24 @@
+﻿using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Input;
+
+namespace WaterSim
+{
+    class Game : GameWindow
+    {
+        public Game(int width, int height, string title) : base(width, height, GraphicsMode.Default, title)
+        {
+
+        }
+
+        protected override void OnUpdateFrame(FrameEventArgs e)
+        {
+            KeyboardState input = Keyboard.GetState();
+            if (input.IsKeyDown(Key.Escape)) Exit();
+
+            base.OnUpdateFrame(e);
+
+        }
+    }
+
+}
--- Program.cs
+++ Program.cs
@@ -6,7 +6,13 @@ namespace WaterSim
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Creating Window");
+            using (Game game = new Game(800, 600, "LearnOpenTK"))
+            {
+                //Run takes a double, which is how many frames per second it should strive to reach.
+                //You can leave that out and it'll just update as fast as the hardware will allow it.
+                game.Run(60.0);
+            }
         }
     }
 }

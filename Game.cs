e0d1c9dafd66baa306759ace92e9f800bbfbae47
Sjimmie <sjimmie.bleeker@student.nhlstenden.com>
2020-08-16 04:35:21 +0200

    HELLO TRIANGLE

--- Game.cs
+++ Game.cs
@@ -1,15 +1,25 @@
 ﻿using OpenTK;
 using OpenTK.Graphics;
+using OpenTK.Graphics.OpenGL;
 using OpenTK.Input;
+using System;
 
 namespace WaterSim
 {
     class Game : GameWindow
     {
-        public Game(int width, int height, string title) : base(width, height, GraphicsMode.Default, title)
-        {
+        private float[] vertices = {
+            -0.5f, -0.5f, 0.0f, //Bottom-left vertex
+             0.5f, -0.5f, 0.0f, //Bottom-right vertex
+             0.0f,  0.5f, 0.0f  //Top vertex
+        };
 
-        }
+        int VertexBufferObject;
+        int VertexArrayObject;
+
+        Shader shader;
+
+        public Game(int width, int height, string title) : base(width, height, GraphicsMode.Default, title) { }
 
         protected override void OnUpdateFrame(FrameEventArgs e)
         {
@@ -18,6 +28,57 @@ namespace WaterSim
 
             base.OnUpdateFrame(e);
 
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            GL.ClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+
+            // Custom code here
+            VertexBufferObject = GL.GenBuffer();
+            GL.BindBuffer(BufferTarget.ArrayBuffer, VertexBufferObject);
+            GL.BufferData(BufferTarget.ArrayBuffer, vertices.Length * sizeof(float), vertices, BufferUsageHint.StaticDraw);
+
+            shader = new Shader("shader.vert", "shader.frag");
+
+            shader.Use();
+
+            VertexArrayObject = GL.GenVertexArray();
+            GL.BindVertexArray(VertexArrayObject);
+            GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 3 * sizeof(float), 0);
+            GL.EnableVertexAttribArray(0);
+            GL.BindBuffer(BufferTarget.ArrayBuffer, VertexBufferObject);
+
+            base.OnLoad(e);
+        }
+
+        protected override void OnRenderFrame(FrameEventArgs e)
+        {
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+
+            // Custom code here
+            shader.Use();
+            GL.BindVertexArray(VertexArrayObject);
+            GL.DrawArrays(PrimitiveType.Triangles, 0, 3);
+
+            Context.SwapBuffers();
+            base.OnRenderFrame(e);
+        }
+
+        protected override void OnResize(EventArgs e)
+        {
+            GL.Viewport(0, 0, Width, Height);
+            base.OnResize(e);
+        }
+
+        protected override void OnUnload(EventArgs e)
+        {
+            GL.BindBuffer(BufferTarget.ArrayBuffer, 0);
+            GL.DeleteBuffer(VertexBufferObject);
+
+            shader.Dispose();
+
+            base.OnUnload(e);
         }
     }
 
--- Program.cs
+++ Program.cs
@@ -7,12 +7,12 @@ namespace WaterSim
         static void Main(string[] args)
         {
             Console.WriteLine("Creating Window");
-            using (Game game = new Game(800, 600, "LearnOpenTK"))
-            {
-                //Run takes a double, which is how many frames per second it should strive to reach.
-                //You can leave that out and it'll just update as fast as the hardware will allow it.
-                game.Run(60.0);
-            }
+            using Game game = new Game(800, 600, "LearnOpenTK");
+
+            //Run takes a double, which is how many frames per second it should strive to reach.
+            //You can leave that out and it'll just update as fast as the hardware will allow it.
+            game.Run(60.0);
+
         }
     }
 }

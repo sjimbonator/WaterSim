2e45f4e2f6976a21946494c1005a57188e13353c
Sjimmie <sjimmie.bleeker@student.nhlstenden.com>
2020-09-10 22:00:21 +0200

    Coordinate system

--- Game.cs
+++ Game.cs
@@ -59,6 +59,20 @@ namespace WaterSim
         private int Texture1;
         private int Texture2;
 
+        private Vector3[] cubePositions =
+        {
+            new Vector3( 0.0f,  0.0f,  0.0f),
+            new Vector3( 2.0f,  5.0f, -15.0f),
+            new Vector3(-1.5f, -2.2f, -2.5f),
+            new Vector3(-3.8f, -2.0f, -12.3f),
+            new Vector3( 2.4f, -0.4f, -3.5f),
+            new Vector3(-1.7f,  3.0f, -7.5f),
+            new Vector3( 1.3f, -2.0f, -2.5f),
+            new Vector3( 1.5f,  2.0f, -2.5f),
+            new Vector3( 1.5f,  0.2f, -1.5f),
+            new Vector3(-1.3f,  1.0f, -1.5f)
+        };
+
         private Shader shader;
 
         private double time;
@@ -126,15 +140,28 @@ namespace WaterSim
 
             shader.Use();
 
-            var model = Matrix4.Identity * Matrix4.CreateRotationX((float)MathHelper.DegreesToRadians(0.5f * (time * 50.0f)));
-            model *= Matrix4.CreateRotationY((float)MathHelper.DegreesToRadians(time * 50.0f));
+            //var model = Matrix4.Identity * Matrix4.CreateRotationX((float)MathHelper.DegreesToRadians(0.5f * (time * 50.0f)));
+            //model *= Matrix4.CreateRotationY((float)MathHelper.DegreesToRadians(time * 50.0f));
+            //shader.SetUniform("model", model);
 
-            shader.SetUniform("model", model);
             shader.SetUniform("view", view);
             shader.SetUniform("projection", projection);
 
             GL.BindVertexArray(VertexArrayObject);
-            GL.DrawArrays(PrimitiveType.Triangles, 0, 36);
+            for (int i = 0; i < 10; i++)
+            {
+                float angle = 20.0f * i;
+                var model = Matrix4.Identity;
+
+                model *= Matrix4.CreateRotationX((float)MathHelper.DegreesToRadians(angle));
+                model *= Matrix4.CreateRotationY((float)MathHelper.DegreesToRadians(0.3 * angle));
+                model *= Matrix4.CreateRotationZ((float)MathHelper.DegreesToRadians(0.5 * angle));
+                model *= Matrix4.CreateTranslation(cubePositions[i]);
+
+                shader.SetUniform("model", model);
+
+                GL.DrawArrays(PrimitiveType.Triangles, 0, 36);
+            }
 
             Context.SwapBuffers();
 

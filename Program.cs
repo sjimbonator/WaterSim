eb4d590fff846886f40b11bd7a1c7765c23dc8fc
Sjimmie <sjimmie.bleeker@student.nhlstenden.com>
2020-09-20 19:52:56 +0200

    Deleted unused file

--- Program.cs
+++ Program.cs
@@ -7,12 +7,8 @@ namespace WaterSim
         static void Main(string[] args)
         {
             Console.WriteLine("Creating Window");
-            using Game game = new Game(1920, 1080, "LearnOpenTK");
-
-            //Run takes a double, which is how many frames per second it should strive to reach.
-            //You can leave that out and it'll just update as fast as the hardware will allow it.
+            using Game game = new Game(1920, 1080, "Terrain Generator");
             game.Run(60.0);
-
         }
     }
 }
